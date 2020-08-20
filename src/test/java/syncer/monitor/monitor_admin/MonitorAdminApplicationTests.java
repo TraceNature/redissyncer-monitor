992c9a90c4aaec924f0949dfbf374a332d450243
平行时空 <1604030622@qq.com>
2020-08-20 17:30:24 +0800

    first commit

---
+++ src/test/java/syncer/monitor/monitor_admin/MonitorAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package syncer.monitor.monitor_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
